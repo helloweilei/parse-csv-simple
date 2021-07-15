94a40cd3bc88c22cba45a426c35ca56993e08ab0
charlie <charlie@charlie.local>
2021-07-15 17:53:29 +0800

    bundle code

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'CSVParser',
+    libraryTarget: 'umd',
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts'],  
+  },
+  plugins: [
+    // new CleanWebpackPlugin(),
+  ]
+}
